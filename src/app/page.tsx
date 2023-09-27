2493ff0ba14c5b6ed0aa0363c36217e2fd2bab1b
KresnaAP <kresnaadhipramana5758@gmail.com>
2023-09-27 22:29:07 +0700

    feat: add home content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,30 @@ export default function Home() {
   return (
     <main className="w-screen min-h-screen">
       <Landing>
-        <Row>
-          <Col span={24} className="flex items-center justofy-center">
-            <div>
-              <h1>Empower your organization's growth</h1>
-              <p></p>
-              <div>
-                <Button type="primary">Primary Button</Button>
-                <Button>Default Button</Button>
-              </div>
-            </div>
+        <Row className="w-screen h-screen">
+          <Col span={24} className="flex items-center justify-center">
+            <Row>
+              <Col>
+                <h1 className="text-center text-8xl font-bold">
+                  Empower your organization's growth
+                </h1>
+                <p>
+                  Our team's design brilliance is matched by our no-code
+                  wizardry, resulting in exceptional products that are both
+                  visually stunning and technically robust - with no-code.
+                </p>
+                <div>
+                  <Button type="primary" className="mx-3">
+                    Primary Button
+                  </Button>
+                  <Button className="mx-3">Default Button</Button>
+                </div>
+              </Col>
+            </Row>
           </Col>
+        </Row>
+        <Row>
+          <Col span={24}></Col>
         </Row>
       </Landing>
     </main>
